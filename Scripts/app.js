a354589e44fb49fe2b3bf986b1512a37af07f353
Jacob Kent <jacobgkent@gmail.com>
2022-02-05 15:27:35 -0500

    Added link icons. Updated Projects function. Updated bottom navbar

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,20 +2,14 @@
 "use strict";
 (function()
 {
-// Todo
+    // Todo
+    // ADD FONTAWESOME ICONS
+    // ADD HR TO NAVBAR
+    // UPDATE ACTIVE PAGE
 
-// Navbar Injection?
+    // DOM MANIPULATION
 
-// DOM MANIPULATION
-
-// Inject text for pages with javascript
-
-// Change "Products" link to "Projects"
-
-// Add HR link between About and Contact in the navbar
-
-// Add fixed bottom navbar with copyright 
-
+    // Top Navbar Injection
     function AddTopNavbar()
     {
         document.getElementsByTagName('header')[0].innerHTML = 
@@ -27,21 +21,21 @@
             <span class="navbar-toggler-icon"></span>
           </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul id="navbarLinks" class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        <a id="navbarHome" class="nav-link active" aria-current="page" href="index.html"><i class="fas fa-home"></i> Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html">About Us</a>
+                        <a id="navbarProducts" class="nav-link" href="products.html"><i class="fas fa-folder"></i> Products</a>
                     </li>
                     <li class="nav-item">
-                        <a id="navbarProducts" class="nav-link" href="products.html">Products</a>
+                        <a id="navbarServices" class="nav-link" href="services.html"><i class="fas fa-dollar-sign"></i> Services</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="services.html">Services</a>
+                        <a id="navbarAboutUs" class="nav-link" href="about.html"><i class="fas fa-info"></i> About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact Us</a>
+                        <a id="navbarContactUs" class="nav-link" href="contact.html"><i class="fas fa-envelope"></i> Contact Us</a>
                     </li>
                 </ul>
                 </div>
@@ -54,27 +48,53 @@
     //This function name is horribly long but it does exactly what it says
     function RenameNavbarProductsToProjects()
     {
-        console.log("Called");
-        document.getElementById("navbarProducts").innerHTML = "Projects";
+        console.log("Called Projects");
+        document.getElementById("navbarProducts").innerHTML = `<i class="fas fa-briefcase"></i> Projects`;
+        document.getElementById("navbarProducts").setAttribute("id", "navbarProjects");
     }
 
+    // Add Human Resourecs between About us and Contact us
+    function InjectHumanResources()
+    {
+        console.log("Called HR");
+        // Get the navbar elements
+        let NavbarLinks = document.getElementById("navbarLinks");
+
+        // Create the element
+        let navbarHR = document.createElement("li");
+
+        // Set its attributes
+        navbarHR.setAttribute("class", "nav-item")
+        navbarHR.innerHTML = `<a id="navbarHumanResources" class="nav-link" href="#HumanResources"><i class="fas fa-user"></i> Human Resources</a>`;
+
+        // Insert it before Contact Us
+        NavbarLinks.insertBefore(navbarHR, NavbarLinks.childNodes[8]);
+    }
+
+    // Add fixed bottom navbar with copyright 
     function AddBottomNavbar()
     {
-        document.body.insertAdjacentHTML("beforeend", 
-        `<nav class="navbar fixed-bottom navbar-light bg-light">
-        <a class="navbar-brand" href="#">&copy Copyright 2022</a>
-        </nav>`)
+        // console.log("Called bottom navbar");
+
+        // Create the element
+        let Navbar = document.createElement("nav");
+
+        // Set its attributes
+        Navbar.setAttribute("class", "navbar fixed-bottom navbar-light bg-light")
+        Navbar.innerHTML = `<a class="navbar-brand" href="#">&copy Copyright 2022</a>`;
+
+        // Append it
+        document.body.appendChild(Navbar);
+
     }
 
     function Start()
     {
         AddTopNavbar();
         AddBottomNavbar();
+        InjectHumanResources();
     }
 
     window.addEventListener("load",Start());
 
-// CONTACT FORM
-
-// Add event listener for submit redirects to home
 })();
